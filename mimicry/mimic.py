003eee2df552d16d953bcdcc47f97b9f4d813ea9
Michael Simpson <mjs2600@gmail.com>
2015-02-24 15:23:07 -0500

    Update MIMIC to look more like the pseudo code from the lecture

--- mimicry/mimic.py
+++ mimicry/mimic.py
@@ -4,13 +4,27 @@ class Mimic(object):
         self.cost_function = cost_function
         self.distribution = Distribution()
 
-    def fit(percentile):
+    def fit(self, percentile):
         pass
-    
-class SampleSet(object)
-    def __init__(bitstring_length):
+
+
+class SampleSet(object):
+    def __init__(self, distribution):
+        self.samples = distribution.generate_samples
+
+    def generate_mutual_information_graph(self):
         pass
+
+    def calculate_fitness(self, fitness_function):
+        pass
+
+    def get_percentile(self, percentile):
+        pass
+
 
 class Distribution(object):
     def __init__(self):
+        pass
+
+    def generate_samples(self):
         pass
