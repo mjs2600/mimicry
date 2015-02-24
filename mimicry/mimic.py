a4d60b275b67b0b79c53bffad1e6cb93918956c6
Michael Simpson <mjs2600@gmail.com>
2015-02-24 14:22:12 -0500

    Sketch out the MIMIC distribution and sample code

--- mimicry/mimic.py
+++ mimicry/mimic.py
@@ -1,6 +1,16 @@
 class Mimic(object):
-    def __init__(self, search_space, cost_function):
-        pass
+    def __init__(self, bitstring_length, cost_function):
+        self.sample_set = SampleSet(bitstring_length)
+        self.cost_function = cost_function
+        self.distribution = Distribution()
 
     def fit(percentile):
+        pass
+    
+class SampleSet(object)
+    def __init__(bitstring_length):
+        pass
+
+class Distribution(object):
+    def __init__(self):
         pass
