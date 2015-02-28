3741041e400797c847851127184108d55c99bc3d
Michael Simpson <mjs2600@gmail.com>
2015-02-27 21:45:09 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mimicry',
-    version='0.1.0',
+    version='0.2.0',
     author='M. Simpson, J. McGehee',
     author_email= 'mjs2600@gmail.com, jlmcgehee21@gmail.com',
     packages=['mimicry'],
